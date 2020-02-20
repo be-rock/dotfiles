6c6b6c18c880c23cfe11d52de1f6621d07b61fdc
be-rock <15165956+be-rock@users.noreply.github.com>
2020-02-19 21:12:14 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # create symlinks if files dont already exist
-for file in .{aliases,exports,functions,misc,sqliterc,vimrc}; do
+for file in .{aliases,exports,functions,inputrc,misc,sqliterc,vimrc}; do
     if [[ ! -r "$HOME/$file" ]] ; then
         echo "Executing: ln -s $HOME/git/dotfiles/$file $HOME/$file ..."
         ln -s $HOME/git/dotfiles/$file $HOME/$file
@@ -11,18 +11,19 @@ done
 unset file
 
 cat << EOF
+
 Other installation considerations:
-  bat - https://github.com/sharkdp/bat
-  tree - sudo apt install tree | brew install tree | yum install tree 
-  oh-my-zsh - https://ohmyz.sh/
+  tree: sudo apt install tree | brew install tree | yum install tree
+  https://github.com/sharkdp/bat
+  https://ohmyz.sh/
+  https://sqlitebrowser.org/
+  https://dbeaver.io
+  https://code.visualstudio.com/Download
+  https://www.jetbrains.com/pycharm/download/
+  https://www.jetbrains.com/idea/download/
+  https://www.postman.com/downloads/
+  https://www.docker.com/products/docker-desktop
+
 EOF
 
-#if [ $(uname -s) = 'Darwin' ]; then
-#    #
-#elif [ $(uname -s) = 'Linux' ] && [ -a /etc/debian_version ]; then
-#    #
-#elif [ $(uname -s) = 'Linux' ] && [ -a /etc/redhat-release ]; then
-#    #
-#fi
-
-#end
+# end
