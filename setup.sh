62511eb961185d91a53c8d78a812e0f694997f46
be-rock <none@gmail.com>
2020-03-25 22:49:08 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -10,23 +10,38 @@ for file in .{aliases,exports,functions,inputrc,misc,sqliterc,vimrc}; do
 done
 unset file
 
+if [ $(uname -s) = 'Darwin' ]; then
+    export LSCOLORS="Gxfxcxdxbxegedabagacad"
+    export SYSTEM_TYPE="macOS"
+elif [ $(uname -s) = 'Linux' ] && [ -f /etc/debian_version ]; then
+    export SYSTEM_TYPE="Linux (Debian)"
+elif [ $(uname -s) = 'Linux' ] && [ -f /etc/redhat-release ]; then
+    export SYSTEM_TYPE="Linux (RedHat)"
+fi
+
+if [ SYSTEM_TYPE="Linux (Debian)" ] && [ INITIAL_INSTALL="YES" ]; then
+    sudo apt update
+    sudo apt install gnome-tweaks
+    sudo apt install wget curl git zsh vim bat tree cheat
+    sudo apt install deja-dup snapd shotwell
+    sudo apt install exfat-fuse exfat-utils
+    sudo snap install hello-world
+    sudo snap install code --classic
+    sudo snap install pycharm-community --classic
+    sudo snap install docker
+    sudo snap install vlc
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+    chsh -s $(which zsh)
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 mkdir -p $HOME/.local/bin
 
 cat << EOF
 
 Other installation considerations:
-  tree: sudo apt install tree | brew install tree | yum install tree
-  bat: apt install bat | brew install bat | https://github.com/sharkdp/bat
-<<<<<<< HEAD
-=======
-  https://docs.conda.io/en/latest/miniconda.html
->>>>>>> 2a80082883c67fae93d715f61927ee75f1435777
-  https://ohmyz.sh/
   https://sqlitebrowser.org/
   https://dbeaver.io
-  https://code.visualstudio.com/Download
-  https://www.jetbrains.com/pycharm/download/
-  https://www.jetbrains.com/idea/download/
   https://www.postman.com/downloads/
   https://www.docker.com/products/docker-desktop
 
