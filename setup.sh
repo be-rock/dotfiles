1413e83716d441ba0a533c0d6135ed314dc73757
be-rock <brock.secondary@gmail.com>
2020-03-28 21:44:42 -0500

    update stuff

--- setup.sh
+++ setup.sh
@@ -22,13 +22,14 @@ fi
 if [ SYSTEM_TYPE="Linux (Debian)" ] && [ INITIAL_INSTALL="YES" ]; then
     sudo apt update
     sudo apt install gnome-tweaks
-    sudo apt install wget curl git zsh vim bat tree cheat
+    sudo apt install wget curl git zsh vim bat tree neofetch
     sudo apt install deja-dup snapd shotwell
     sudo apt install exfat-fuse exfat-utils
     sudo snap install hello-world
     sudo snap install code --classic
-    sudo snap install pycharm-community --classic
     sudo snap install docker
+    sudo snap install libreoffice
+    sudo snap install pycharm-community --classic
     sudo snap install vlc
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
     chsh -s $(which zsh)
