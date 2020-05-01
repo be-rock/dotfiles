7678215f96622cae59a65265c3a309a2d1a8874e
be-rock <brock.secondary@gmail.com>
2020-04-30 23:22:40 -0500

    update

--- setup.sh
+++ setup.sh
@@ -10,33 +10,95 @@ for file in .{aliases,exports,functions,inputrc,misc,sqliterc,vimrc}; do
 done
 unset file
 
-if [ $(uname -s) = 'Darwin' ]; then
-    export LSCOLORS="Gxfxcxdxbxegedabagacad"
-    export SYSTEM_TYPE="macOS"
-elif [ $(uname -s) = 'Linux' ] && [ -f /etc/debian_version ]; then
-    export SYSTEM_TYPE="Linux (Debian)"
-elif [ $(uname -s) = 'Linux' ] && [ -f /etc/redhat-release ]; then
-    export SYSTEM_TYPE="Linux (RedHat)"
-fi
 
-if [ SYSTEM_TYPE="Linux (Debian)" ] && [ INITIAL_INSTALL="YES" ]; then
+source $HOME/git/dotfiles/.exports
+source $HOME/git/dotfiles/.functions
+
+function debian_setup () {
     sudo apt update
-    sudo apt install gnome-tweaks
-    sudo apt install bat curl git neofetch tree vim wget xclip zsh
-    sudo apt install deja-dup snapd shotwell
-    sudo apt install exfat-fuse exfat-utils
-    sudo snap install hello-world
-    sudo snap install code --classic
-    sudo snap install docker
-    sudo snap install libreoffice
-    sudo snap install pycharm-community --classic
-    sudo snap install vlc
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-    chsh -s $(which zsh)
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
 
-mkdir -p $HOME/.local/bin
+    sudo apt install \
+        bat \
+        curl \
+        deja-dup \
+        exfat-fuse \
+        exfat-utils \
+        jq \
+        git \
+        gnome-tweaks \
+        neofetch \
+        shotwell \
+        snapd \
+        sqlitebrowser \
+        tree \
+        vim \
+        wget \
+        xclip \
+        zsh
+
+    sudo snap install \
+        hello-world \
+        code --classic \
+        docker \
+        libreoffice \
+        pycharm-community --classic \
+        intellij-idea-community --classic \
+        vlc
+}
+
+function mac_setup() {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+    brew install \
+        bat \
+        jq \
+        tree \
+        zsh
+
+    brew cask install \
+        docker \
+        rectangle
+}
+
+function ohmyzsh_setup() {
+    sudo chsh -s $(which zsh)
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+function python_setup() {
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+    bash ~/miniconda.sh -b -p $HOME/miniconda
+    ln -s $HOME/miniconda/bin/conda $HOME/.local/bin/conda
+    $HOME/miniconda/bin/conda init zsh
+    exec /usr/bin/zsh -l
+    $HOME/miniconda/bin/conda activate base
+    
+    python -m venv $HOME/.venv/misc
+    $HOME/.venv/misc/bin/pip install \
+        airflow
+}
+
+
+function runner() {
+    if [ "$INITIAL_INSTALL" = "YES" ]; then 
+        mkdir -p $HOME/.local/bin $HOME/.venv/misc
+        export PATH=$HOME/.local/bin:$PATH
+
+        if [ SYSTEM_TYPE="Linux (Debian)" ]; then 
+            echo 'debian setup...'
+            debian_setup
+        elif [ SYSTEM_TYPE="macOS" ]; then 
+            echo 'mac setup...'
+            mac_setup
+        fi
+
+        ohmyzsh_setup
+        python_setup
+    fi
+}
+
+runner
+
 
 cat << EOF
 
