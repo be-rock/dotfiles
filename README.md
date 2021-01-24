a9456b50e05066d8f0a094b3b9152f4359adc811
brock <brock@pop-os.localdomain>
2021-01-24 13:54:59 -0600

    ansible updates

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # dotfiles
+
+## Getting Started
+
+### Mac
+
+1. Install homebrew with the command from the site
+2. Install miniconda with the command from the site
+3. `pip install ansible`
+4. Run the playbook
+
+### Linux
+
+1. Install miniconda with the command from the site
+2. `pip install ansible`
+3. Run the playbook
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,151 +1,153 @@
 #!/usr/bin/env bash
 
-function initial_setup() {
-    source $PWD/.exports
-    source $PWD/.functions
+#ansible-playbook .ansible/dotfiles.yaml -i .hosts.ini
 
-    print_msg "creating directories"
-    mkdir -p $HOME/{.cache,.config,.local/{bin,lib,share},code/{git,python,sql},go}
-    ln -s $HOME/go $HOME/code/go
-
-    print_msg "create symlinks if files dont already exist..."
-    for file in .{aliases,ansible.cfg,exports,functions,inputrc,misc,sqliterc,starship.toml,vimrc}; do
-        if [[ ! -r "$HOME/$file" ]] ; then
-            print_msg "Executing: ln -s $PWD/$file $HOME/$file ..."
-            ln -s $PWD/$file $HOME/$file
-            print_msg "Return code: $?"
-
-            print_msg "Executing: ln -s $PWD/$file $HOME/.config/$file ..."
-            ln -s $PWD/$file $HOME/.config/$file
-            print_msg "Return code: $?"
-        fi
-    done
-    unset file
-}
-
-function debian_base_setup () {
-    print_msg "debian_base_setup..."
-    sudo apt update
-
-    sudo apt install -y \
-        bat \
-        curl \
-        exfat-fuse \
-        exfat-utils \
-        jq \
-        git \
-        neofetch \
-        pipx \
-        ripgrep \
-        snapd \
-        tldr \
-        tree \
-        vim \
-        wget \
-        xclip \
-        zsh
-
-    ln -s /usr/bin/batcat ~/.local/bin/bat
-
-}
-
-function debian_gnome_setup () {
-    print_msg "debian_gnome_setup..."
-    sudo apt update
-
-    sudo apt install -y \
-        chromium \
-        # deja-dup \
-        gnome-shell-pomodoro \
-        gnome-tweaks \
-        # shotwell \
-        snapd
-
-    sudo snap install \
-        hello-world \
-        code --classic \
-        docker \
-        libreoffice \
-        pycharm-community --classic \
-        intellij-idea-community --classic \
-        vlc
-
-}
-
-function mac_setup() {
-    print_msg "mac_setup..."
-
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-    brew install \
-        bat \
-        jq \
-        pipx \
-        starship \
-        tree \
-        zsh
-
-    brew cask install \
-        docker \
-        maccy \
-        rectangle
-}
-
-function ohmyzsh_setup() {
-    print_msg "ohmyzsh_setup..."
-    sudo chsh -s $(which zsh)
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-    chsh -s $(which zsh)
-}
-
-function starship_setup() {
-    print_msg "starship_setup..."
-    curl -fsSL https://starship.rs/install.sh | bash
-}
-
-function python_setup() {
-    print_msg "python setup..."
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-    bash ~/miniconda.sh -b -p $HOME/miniconda
-    $HOME/miniconda/bin/conda init zsh
-    exec /usr/bin/zsh -l
-    $HOME/miniconda/bin/conda activate base
-}
-
-function pipx_setup() {
-    pipx install \
-        ansible --include-deps
-        #awscli \
-        black \
-        bpytop \
-        #cookiecutter \
-        httpie \
-        tldr
-}
-
-
-function runner() {
-    if [ "$INITIAL_INSTALL" = "YES" ]; then
-        mkdir -p $HOME/.local/bin
-        export PATH=$HOME/.local/bin:$PATH
-
-        if [ SYSTEM_TYPE="Linux (Debian)" ]; then
-            debian_base_setup
-            #debian_gnome_setup
-        elif [ SYSTEM_TYPE="macOS" ]; then
-            mac_setup
-        fi
-    fi
-
-    #ohmyzsh_setup
-    #starship_setup
-    #python_setup
-
-}
-
-#initial_setup
-
-#runner
-
-# end
+#function initial_setup() {
+#    source $PWD/.exports
+#    source $PWD/.functions
+#
+#    print_msg "creating directories"
+#    mkdir -p $HOME/{.cache,.config,.local/{bin,lib,share},code/{git,python,sql},go}
+#    ln -s $HOME/go $HOME/code/go
+#
+#    print_msg "create symlinks if files dont already exist..."
+#    for file in .{aliases,ansible.cfg,exports,functions,inputrc,misc,sqliterc,starship.toml,vimrc}; do
+#        if [[ ! -r "$HOME/$file" ]] ; then
+#            print_msg "Executing: ln -s $PWD/$file $HOME/$file ..."
+#            ln -s $PWD/$file $HOME/$file
+#            print_msg "Return code: $?"
+#
+#            print_msg "Executing: ln -s $PWD/$file $HOME/.config/$file ..."
+#            ln -s $PWD/$file $HOME/.config/$file
+#            print_msg "Return code: $?"
+#        fi
+#    done
+#    unset file
+#}
+#
+#function debian_base_setup () {
+#    print_msg "debian_base_setup..."
+#    sudo apt update
+#
+#    sudo apt install -y \
+#        bat \
+#        curl \
+#        exfat-fuse \
+#        exfat-utils \
+#        jq \
+#        git \
+#        neofetch \
+#        pipx \
+#        ripgrep \
+#        snapd \
+#        tldr \
+#        tree \
+#        vim \
+#        wget \
+#        xclip \
+#        zsh
+#
+#    ln -s /usr/bin/batcat ~/.local/bin/bat
+#
+#}
+#
+#function debian_gnome_setup () {
+#    print_msg "debian_gnome_setup..."
+#    sudo apt update
+#
+#    sudo apt install -y \
+#        chromium \
+#        # deja-dup \
+#        gnome-shell-pomodoro \
+#        gnome-tweaks \
+#        # shotwell \
+#        snapd
+#
+#    sudo snap install \
+#        hello-world \
+#        code --classic \
+#        docker \
+#        libreoffice \
+#        pycharm-community --classic \
+#        intellij-idea-community --classic \
+#        vlc
+#
+#}
+#
+#function mac_setup() {
+#    print_msg "mac_setup..."
+#
+#    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#
+#    brew install \
+#        bat \
+#        jq \
+#        pipx \
+#        starship \
+#        tree \
+#        zsh
+#
+#    brew cask install \
+#        docker \
+#        maccy \
+#        rectangle
+#}
+#
+#function ohmyzsh_setup() {
+#    print_msg "ohmyzsh_setup..."
+#    sudo chsh -s $(which zsh)
+#    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+#    chsh -s $(which zsh)
+#}
+#
+#function starship_setup() {
+#    print_msg "starship_setup..."
+#    curl -fsSL https://starship.rs/install.sh | bash
+#}
+#
+#function python_setup() {
+#    print_msg "python setup..."
+#    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+#    bash ~/miniconda.sh -b -p $HOME/miniconda
+#    $HOME/miniconda/bin/conda init zsh
+#    exec /usr/bin/zsh -l
+#    $HOME/miniconda/bin/conda activate base
+#}
+#
+#function pipx_setup() {
+#    pipx install \
+#        ansible --include-deps
+#        #awscli \
+#        black \
+#        bpytop \
+#        #cookiecutter \
+#        httpie \
+#        tldr
+#}
+#
+#
+#function runner() {
+#    if [ "$INITIAL_INSTALL" = "YES" ]; then
+#        mkdir -p $HOME/.local/bin
+#        export PATH=$HOME/.local/bin:$PATH
+#
+#        if [ SYSTEM_TYPE="Linux (Debian)" ]; then
+#            debian_base_setup
+#            #debian_gnome_setup
+#        elif [ SYSTEM_TYPE="macOS" ]; then
+#            mac_setup
+#        fi
+#    fi
+#
+#    #ohmyzsh_setup
+#    #starship_setup
+#    #python_setup
+#
+#}
+#
+##initial_setup
+#
+##runner
+#
+## end
