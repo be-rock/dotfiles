3b2d59df23ab486d6457b61365910bf1d8dd8050
be-rock <brock.secondary@gmail.com>
2021-12-06 11:37:46 -0600

    add ipython startup scripts

---
+++ ipython/startup_commands.py
@@ -0,0 +1,13 @@
+#
+# put this file in ~/.ipython/profile_default/startup
+# to make the below functions immediately usable upon creation of new ipython sessions
+# with a 'default' profile
+#
+
+import datetime
+
+def aws_ec2_format_dns_name(dns_name: str) -> str:
+    return '.'.join(dns_name.split('.')[0].split('-')[1:])
+
+def now() -> str:
+    return datetime.datetime.now(datetime.timezone.utc).astimezone().isoformat()
