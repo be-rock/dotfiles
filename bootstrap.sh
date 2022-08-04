e17f23d93f40b54df896eeaf58f68f893b440581
be-rock <brock.secondary@gmail.com>
2022-08-04 12:28:24 -0500

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 DOTFILES_DIR="${DOTFILES_DIR:-~/code/git/dotfiles}"
+DESKTOP_OS_DISTRIBUTION="${DESKTOP_OS_DISTRIBUTION:-'Pop!_OS'}"
 
-ansible-playbook ansible/playbook.yml -i ansible/hosts.ini --extra-vars dotfiles_dir=$DOTFILES_DIR
+ansible-playbook ansible/playbook.yml -i ansible/hosts.ini && \
+    --extra-vars dotfiles_dir=$DOTFILES_DIR && \
+    --extra-vars desktop_os_distribution=$DESKTOP_OS_DISTRIBUTION && \
+    --extra-vars pihole_ip=$PIHOLE_IP
