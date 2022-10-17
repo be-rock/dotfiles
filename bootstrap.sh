7f62ddd434142f0cefe6bc4e48cdc82996d91cf9
be-rock <brock.secondary@gmail.com>
2022-10-16 21:45:51 -0500

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 DOTFILES_DIR="${DOTFILES_DIR:-~/code/git/dotfiles}"
-PIHOLE_IP="${PIHOLE_IP:-'localhost'}"
 
 # run a specific role with --tags roleName
-ansible-playbook ansible/playbook.yml -i ansible/hosts.ini --extra-vars dotfiles_dir=$DOTFILES_DIR --extra-vars pihole_ip=$PIHOLE_IP
+ansible-playbook ansible/playbook.yml -i ansible/hosts.ini \
+    --extra-vars dotfiles_dir=$DOTFILES_DIR \
+    --tags apt
